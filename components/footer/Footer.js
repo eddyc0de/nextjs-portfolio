531be8024511cf2168bd40c61bad2d1bf613ed99
Edgar <edgar.pedreno@gmail.com>
2022-01-20 18:29:43 +0100

    Add responsive to the footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,9 +1,18 @@
 import styles from './Footer.module.css';
+import { Row, Col, Container } from 'react-bootstrap';
 
 const Footer = () => {
-  return <footer className={styles.footer}>
-    <p>Site desgined and created by Edgar. Copyright &copy; 2022.</p>
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      <Container className='d-flex justify-content-center align-items-center h-100'>
+        <Row className=''>
+          <Col>
+            <p>Site desgined and created by Edgar. Copyright &copy; 2022.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
